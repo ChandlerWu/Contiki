b7cf2d5ef0adfbb90af0fae57dccce31db8ee0da
Daniel Benson <djbenson@wpi.edu>
2015-10-09 05:42:07 +0200

    Fixed the readline problem. Have not tested it but I'm relatively sure that it will work.

--- plot_trickle.py
+++ plot_trickle.py
@@ -12,24 +12,27 @@ import matplotlib.pyplot as plt
  
  
 from collections import defaultdict
-cpuOverTime =  defaultdict(list)
-i = 0
-s = 0
- 
-with open('failed one.txt', 'rb') as f:
-	reader = csv.reader(f,delimiter=' ')
-	for row in reader:
-		i = i + 1
-		#print i, row[0],row[1],row[2]
-		if 'Consistent RX' not in row[1]:
-			if row[2] is 'P':
-				cpuOverTime[row[3]].append(row[18][:-1])
-				if i > 800:
-					s = s + float(row[18][:-1])
 
-print s/ (i-800)
- 
-for i in cpuOverTime:
-	plt.plot(cpuOverTime[i])
-plt.show()
+def main():
+	cpuOverTime =  defaultdict(list)
+	i = 0
+	s = 0
+
+	f = file.open("failed one.txt")
+	for line in f:
+		reader = csv.reader(line,delimiter=' ')
+		for row in reader:
+			i = i + 1
+			print i, row[0],row[1],row[2]
+			if 'Consistent RX' not in row[1]:
+				if row[2] is 'P':
+					cpuOverTime[row[3]].append(row[18][:-1])
+					if i > 800:
+						s = s + float(row[18][:-1])
+
+	print s / (i-800)
+	 
+	for i in cpuOverTime:
+		plt.plot(cpuOverTime[i])
+	plt.show()
 ########## 
